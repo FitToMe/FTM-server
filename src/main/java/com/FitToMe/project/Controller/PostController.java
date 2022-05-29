98faa470f422e13edea52443f704147de4b31651
brenda <brenda524@naver.com>
2022-05-30 01:35:03 +0900

    feat: add post controller (#1)

--- src/main/java/com/FitToMe/project/Controller/PostController.java
+++ src/main/java/com/FitToMe/project/Controller/PostController.java
@@ -1,5 +1,7 @@
 package com.FitToMe.project.Controller;
 
+import com.FitToMe.project.ApiResult.ApiResult;
+import com.FitToMe.project.DTO.PostDTO;
 import com.FitToMe.project.Request.PostRequest;
 import com.FitToMe.project.Service.Post.PostDeleteService;
 import com.FitToMe.project.Service.Post.PostModifyService;
@@ -7,6 +9,10 @@ import com.FitToMe.project.Service.Post.PostRegisterService;
 import com.FitToMe.project.Service.Post.PostStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/post")
@@ -20,32 +26,33 @@ public class PostController {
 
     // 전체 게시글 조회
     @GetMapping("/")
-    public void readList(){
-        postStatusService.findPosts();
+    public ApiResult<List<PostDTO>> readPostList(){
+
+        // check
+        return ApiResult.SUCCESS(postStatusService.findPosts().stream().map(PostDTO::new).collect(Collectors.toList()));
     }
 
     // 특정 게시글 조회
-    @GetMapping("/{post_id}")
-    public void readOne(@PathVariable("post_id") Long postId){
-        postStatusService.findPostById(postId);
-    }
+//    @GetMapping("/{post_id}")
+//    public ApiResult<PostDTO> readPostOnlyOne(@PathVariable("post_id") Long postId){
+//        return ApiResult.SUCCESS(postStatusService.findPostById(postId));
+//    }
 
     // 게시글 등록
     @PostMapping("/")
-    public void create(PostRequest postRequest){
-        postRegisterService.createPost(postRequest);
+    public ApiResult<PostDTO> createPost(@Valid @RequestBody PostRequest postRequest){
+        return ApiResult.SUCCESS(postRegisterService.createPost(postRequest));
     }
 
     // 게시글 수정
     @PutMapping("/{post_id}")
-    public void update(@PathVariable("post_id") Long postId, PostRequest postRequest){
-        postModifyService.updatePost(postRequest);
+    public ApiResult<PostDTO> updatePost(@Valid @RequestBody PostRequest postRequest){
+        return ApiResult.SUCCESS(postModifyService.updatePost(postRequest));
     }
 
     // 게시글 삭제
     @DeleteMapping("/{post_id}")
-    public void delete(@PathVariable("post_id") Long postId){
-        postDeleteService.deletePost(postId);
+    public ApiResult<Boolean> deletePost(@PathVariable("post_id") Long postId){
+        return ApiResult.SUCCESS(postDeleteService.deletePost(postId));
     }
-
 }
