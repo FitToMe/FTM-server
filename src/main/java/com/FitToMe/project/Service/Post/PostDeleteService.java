24a308a14d7c557c370d441f9854c469d609bad2
chisan01 <chisan2001@gmail.com>
2022-07-21 23:02:33 +0900

    feat: post 관련 class에서 user 관련 기능 수정

--- src/main/java/com/FitToMe/project/Service/Post/PostDeleteService.java
+++ src/main/java/com/FitToMe/project/Service/Post/PostDeleteService.java
@@ -1,6 +1,7 @@
 package com.FitToMe.project.Service.Post;
 
 import com.FitToMe.project.Entity.Post;
+import com.FitToMe.project.Entity.User;
 import com.FitToMe.project.Repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,13 @@ public class PostDeleteService {
 
     private final PostRepository postRepository;
 
-    public boolean deletePost(Long postId) throws RuntimeException {
+    public boolean deletePost(User user, Long postId) throws RuntimeException {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다"));
+
+        if (!post.getUser().equals(user)) {
+            throw new SecurityException("해당 게시글을 삭제할 수 있는 권한이 없습니다");
+        }
 
         postRepository.deleteById(postId);
         return true;
--- src/main/java/com/FitToMe/project/Service/Post/PostRegisterService.java
+++ src/main/java/com/FitToMe/project/Service/Post/PostRegisterService.java
@@ -2,6 +2,7 @@ package com.FitToMe.project.Service.Post;
 
 import com.FitToMe.project.DTO.PostDTO;
 import com.FitToMe.project.Entity.Post;
+import com.FitToMe.project.Entity.User;
 import com.FitToMe.project.Repository.PostRepository;
 import com.FitToMe.project.Request.PostRegisterRequest;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,11 @@ public class PostRegisterService {
 
     private final PostRepository postRepository;
 
-    public PostDTO createPost(PostRegisterRequest postRegisterRequest) {
-        return new PostDTO(postRepository.save(new Post(postRegisterRequest)));
+    public PostDTO createPost(User user, PostRegisterRequest postRegisterRequest) {
+        if (user == null) {
+            throw new RuntimeException("로그인이 필요합니다");
+        }
+
+        return new PostDTO(postRepository.save(new Post(user, postRegisterRequest)));
     }
 }
