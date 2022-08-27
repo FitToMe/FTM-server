4687fc3277b084b4571275b91a7715af781dcf72
alswnsp411 <alswnsp411@naver.com>
2022-08-28 01:31:48 +0900

    CORS policy 설정

---
+++ src/main/java/com/FitToMe/project/Config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.FitToMe.project.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//CORS 설정
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.DELETE.name()
+                );
+    }
+}
