820c0b5762c172f54342c14dc9cecc85a32e9146
JinYoungLee <brenda524@naver.com>
2022-09-06 19:54:08 +0900

    feat: 기존 Post 엔터티를 클래스,소모임,커뮤니티 총 3개의 Post 엔터티로 분류

---
+++ src/main/java/com/FitToMe/project/Entity/CommunityPost.java
@@ -0,0 +1,38 @@
+package com.FitToMe.project.Entity;
+
+import com.FitToMe.project.Request.PostRegisterRequest;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommunityPost extends TimeBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String imageURL;
+    private Integer likeCnt;
+    private Integer viewCnt;
+
+    public CommunityPost(User user, PostRegisterRequest postRegisterRequest) {
+        this.user = user;
+        this.title = postRegisterRequest.getTitle();
+        this.content = postRegisterRequest.getContent();
+        this.imageURL = postRegisterRequest.getImageURL();
+    }
+}
---
+++ src/main/java/com/FitToMe/project/Entity/SmallGroupPost.java
@@ -0,0 +1,42 @@
+package com.FitToMe.project.Entity;
+
+import com.FitToMe.project.Request.PostRegisterRequest;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SmallGroupPost extends TimeBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String imageURL;
+    private Integer totalParticipant;
+    private Integer participantNum;
+    private Integer likeCnt;
+    private Integer viewCnt;
+    private Integer cost;
+    private Boolean isExecuting;
+
+    public SmallGroupPost(User user, PostRegisterRequest postRegisterRequest) {
+        this.user = user;
+        this.title = postRegisterRequest.getTitle();
+        this.content = postRegisterRequest.getContent();
+        this.imageURL = postRegisterRequest.getImageURL();
+    }
+}
