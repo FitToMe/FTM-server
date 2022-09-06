ebf31ab7ed95327f829d2faef30193ba83f009a8
Jinyoung <74577889+jinyoung-2@users.noreply.github.com>
2022-09-06 20:30:58 +0900

    feat: 기존 Post 엔터티를 클래스,소모임,커뮤니티 총 3개의 Post 엔터티로 분류 (#23)
    * feat: 기존 Post 엔터티를 클래스,소모임,커뮤니티 총 3개의 Post 엔터티로 분류
    
    * refactor: 모집중에 대한 직관적 표현으로 변수명 수정
    
    * refactor: Post 엔터티에서 likeCnt 멤버 변수 제거

---
+++ src/main/java/com/FitToMe/project/Entity/ClassPost.java
@@ -0,0 +1,41 @@
+package com.FitToMe.project.Entity;
+
+import com.FitToMe.project.Request.PostRegisterRequest;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClassPost extends TimeBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String imageURL;
+    private Integer totalParticipant;
+    private Integer participantNum;
+    private Integer viewCnt;
+    private Integer cost;
+    private Boolean isRecruiting;
+
+    public ClassPost(User user, PostRegisterRequest postRegisterRequest) {
+        this.user = user;
+        this.title = postRegisterRequest.getTitle();
+        this.content = postRegisterRequest.getContent();
+        this.imageURL = postRegisterRequest.getImageURL();
+    }
+}
