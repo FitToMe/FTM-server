4916034da6916d691f4c0e466b19ab1f640d6944
JinYoungLee <brenda524@naver.com>
2022-09-11 18:17:42 +0900

    feat: 댓글 엔터티에 annotation 추가 및 날짜와 닉네임 멤버변수 추가

--- src/main/java/com/FitToMe/project/DTO/Comment/SmallGroupCommentDTO.java
+++ src/main/java/com/FitToMe/project/DTO/Comment/SmallGroupCommentDTO.java
@@ -1,19 +1,31 @@
 package com.FitToMe.project.DTO.Comment;
 
 import com.FitToMe.project.Entity.Comment.SmallGroupComment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
+@Data
 @Getter
 @NoArgsConstructor
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class SmallGroupCommentDTO {
 
     private Long id;
     private String content;
 
-    public SmallGroupCommentDTO(SmallGroupComment comment){
-        this.id = comment.getId();
-        this.content = comment.getContent();
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+
+    private String authorNickname;
+
+    public SmallGroupCommentDTO(SmallGroupComment entity) {
+        this.id = entity.getId();
+        this.content = entity.getContent();
+        this.regDate = entity.getRegDate();
+        this.modDate = entity.getModDate();
+        this.authorNickname = entity.getUser().getNickname();
     }
 }
